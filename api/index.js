d3c16c4364eed2100b7a5ace16a1c67bbcab6bcc
ManiacD16 <msd161918@gmail.com>
2024-09-10 11:24:03 +0530

    Initial commit

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
+const {
+  createServer,
+  IncomingMessage,
+  request,
+  request,
+  ServerResponse,
+} = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-// Initialize the Express app
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
   console.error("MongoDB URI is not defined in environment variables.");
-  process.exit(1); // Exit if URI is not set
+  process.exit(1);
 }
 
 mongoose
@@ -21,10 +25,9 @@ mongoose
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit the process if connection fails
+    process.exit(1);
   });
 
-// Wallet Schema and Model
 const WalletSchema = new mongoose.Schema({
   address: { type: String, required: true, unique: true },
   balance: { type: String },
@@ -33,14 +36,13 @@ const WalletSchema = new mongoose.Schema({
 
 const Wallet = mongoose.model("Wallet", WalletSchema);
 
-// Route to save wallet information
 app.post("/api/wallet", async (req, res) => {
   const { address, balance } = req.body;
-  console.log("Received data from frontend:", { address, balance }); // Log received data
+  console.log("Received data from frontend:", { address, balance });
 
   try {
     let wallet = await Wallet.findOne({ address });
-    console.log("Checking if wallet exists:", wallet); // Log if wallet is found or not
+    console.log("Checking if wallet exists:", wallet);
 
     if (!wallet) {
       console.log("Wallet not found. Creating new wallet.");
@@ -48,18 +50,14 @@ app.post("/api/wallet", async (req, res) => {
       await wallet.save();
       console.log("New wallet saved:", wallet);
     } else {
-      console.log("Wallet already exists:", wallet); // Log existing wallet
+      console.log("Wallet already exists:", wallet);
     }
 
     res.json(wallet);
   } catch (err) {
-    console.error("Error saving wallet data:", err); // Log detailed error
+    console.error("Error saving wallet data:", err);
     res.status(500).send("Server error");
   }
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
